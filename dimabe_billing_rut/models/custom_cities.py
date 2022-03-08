e7e54aeaa7b8d459711eedad1b3ba83da7477020
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-03-08 15:23:27 -0300

    remove warnings

--- dimabe_billing_rut/models/custom_cities.py
+++ dimabe_billing_rut/models/custom_cities.py
@@ -17,3 +17,4 @@ class CustomCities(models.Model):
         for item in self:
             if item.name and item.country:
                 item.city_country = item.name + ', ' +  item.country.name
+
