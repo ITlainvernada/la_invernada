c1cdaf93ec4381f3ea7c622266dee04cd1a61e60
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-03-08 13:08:31 -0300

    remove warnings

--- dimabe_billing_rut/models/custom_freight_payment_term.py
+++ dimabe_billing_rut/models/custom_freight_payment_term.py
@@ -3,5 +3,6 @@ from odoo import models, fields, api
 class CustomFreightPaymentTerm(models.Model):
 
     _name = 'custom.freight.payment.term'
+    _description = "Plazo de Pago Flete"
 
     name = fields.Char(string= 'Nombre', required=True)
