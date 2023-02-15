2a034da7478971212fdd51b5977bb63b6d736cdb
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2023-02-15 10:06:13 -0300

    test spring 3

--- dimabe_billing_rut/models/stock_picking.py
+++ dimabe_billing_rut/models/stock_picking.py
@@ -1,18 +1,20 @@
-from odoo import models, fields, api
-import json
-import requests
-from datetime import date
-import re
-from pdf417 import encode, render_image, render_svg
 import base64
-from io import BytesIO 
+import json
+import re
+from io import BytesIO
 from math import floor
+
+import requests
+from pdf417 import encode, render_image
+
+from odoo import models, fields, api
+
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
     dte_folio = fields.Text(string='Folio DTE')
-    dte_type_id =  fields.Many2one(
-        'dte.type', string = 'Tipo Documento'
+    dte_type_id = fields.Many2one(
+        'dte.type', string='Tipo Documento'
     )
     dte_xml = fields.Text("XML")
     dte_xml_sii = fields.Text("XML SII")
@@ -20,7 +22,7 @@ class StockPicking(models.Model):
     ted = fields.Text("TED")
     pdf_url = fields.Text("URL PDF")
 
-    partner_economic_activities = fields.Many2many('custom.economic.activity',related='partner_id.economic_activities')
+    partner_economic_activities = fields.Many2many('custom.economic.activity', related='partner_id.economic_activities')
     company_economic_activities = fields.Many2many('custom.economic.activity', related='company_id.economic_activities')
     partner_activity_id = fields.Many2one('custom.economic.activity', string='Actividad del Cliente/Proveedor')
     company_activity_id = fields.Many2one('custom.economic.activity', string='Actividad de la Compañía')
@@ -44,26 +46,25 @@ class StockPicking(models.Model):
         default='1',
     )
 
-    observations_ids = fields.One2many('custom.invoice.observations','invoice_id',string='Observaciones')
+    observations_ids = fields.One2many('custom.invoice.observations', 'invoice_id', string='Observaciones')
 
     dispatch_type = fields.Selection([
-            ('0', 'Sin Despacho'),
-            ('1', 'Despacho por cuenta del receptor del documento'),
-            ('2', 'Despacho por cuenta del emisor a instalaciones del cliente'),
-            ('3', 'Despacho por cuenta del emisor a otras instalaciones')
-            ], 'Tipo de Despacho', default='0')
+        ('0', 'Sin Despacho'),
+        ('1', 'Despacho por cuenta del receptor del documento'),
+        ('2', 'Despacho por cuenta del emisor a instalaciones del cliente'),
+        ('3', 'Despacho por cuenta del emisor a otras instalaciones')
+    ], 'Tipo de Despacho', default='0')
 
-    transfer_indication  = fields.Selection([
-            ('0', 'Sin Translado'),
-            ('1', 'Operación constituye venta'),
-            ('2', 'Ventas por efectuar'),
-            ('3', 'Consignaciones'),
-            ('4', 'Entrega gratuita'),
-            ('5', 'Traslados internos'),
-            ('6', 'Otros traslados no venta'),
-            ('7', 'Guía de devolución'),
-            ], 'Tipo Translado', default='0')
-
+    transfer_indication = fields.Selection([
+        ('0', 'Sin Translado'),
+        ('1', 'Operación constituye venta'),
+        ('2', 'Ventas por efectuar'),
+        ('3', 'Consignaciones'),
+        ('4', 'Entrega gratuita'),
+        ('5', 'Traslados internos'),
+        ('6', 'Otros traslados no venta'),
+        ('7', 'Guía de devolución'),
+    ], 'Tipo Translado', default='0')
 
     date_due = fields.Date(string="Fecha Vencimiento")
 
@@ -75,11 +76,11 @@ class StockPicking(models.Model):
 
     total = fields.Char(string="Total")
 
-    invoiced = fields.Boolean(string="Estado Facturación",default=False)
+    invoiced = fields.Boolean(string="Estado Facturación", default=False)
 
     valid_to_sii = fields.Boolean(string="Valido para SII")
 
-    #Comex Embarque
+    # Comex Embarque
 
     shipping_company = fields.Many2one(
         comodel_name='custom.shipping.company',
@@ -98,7 +99,7 @@ class StockPicking(models.Model):
         copy=False
     )
 
-    type_transport = fields.Many2one('custom.type.transport','Vía de Transporte',copy=False)
+    type_transport = fields.Many2one('custom.type.transport', 'Vía de Transporte', copy=False)
 
     departure_port = fields.Many2one(
         comodel_name='custom.port',
@@ -148,13 +149,19 @@ class StockPicking(models.Model):
         copy=False
     )
 
-    departure_date = fields.Datetime('Fecha de zarpe',copy=False)
+    departure_date = fields.Datetime('Fecha de zarpe', copy=False)
 
-    arrival_date = fields.Datetime('Fecha de arribo',copy=False)
+    arrival_date = fields.Datetime('Fecha de arribo', copy=False)
 
-    #Comex Embarque Method
+    # Comex Embarque Method
 
-    use_documents = fields.Boolean('Usa documentos', default=True)
+    use_documents = fields.Boolean('Usa documentos')
+
+    @api.onchange('picking_type_id')
+    def onchange_documents(self):
+        for item in self:
+            if item.picking_type_id.code == 'outgoing':
+                item.use_documents = True
 
     @api.model
     @api.onchange('etd')
@@ -194,7 +201,6 @@ class StockPicking(models.Model):
         if self.eta and self.eta < self.etd:
             raise models.ValidationError('La ETA debe ser mayor al ETD')
 
-
     @api.onchange('partner_id')
     @api.multi
     def _compute_partner_activity(self):
@@ -203,14 +209,13 @@ class StockPicking(models.Model):
             for activity in item.partner_id.economic_activities:
                 activities.append(activity.id)
             item.partner_activity_id = activities
-    
 
     @api.one
     def send_to_sii(self):
 
         url = self.env.user.company_id.dte_url
         headers = {
-            "apiKey" : self.env.user.company_id.dte_hash,
+            "apiKey": self.env.user.company_id.dte_hash,
             "CustomerCode": self.env.user.company_id.dte_customer_code
         }
         invoice = {}
@@ -219,9 +224,8 @@ class StockPicking(models.Model):
         netAmount = 0
         exemtAmount = 0
         countNotExempt = 0
-        
 
-        #Main Validations
+        # Main Validations
         self.validation_fields()
 
         move_line = []
@@ -232,17 +236,20 @@ class StockPicking(models.Model):
 
         for item in move_line:
             haveExempt = False
-            
-            price_unit =  item.sale_id.mapped('order_line').filtered(lambda a : a.product_id.id == item.product_id.id).price_unit if self.is_multiple_dispatch else item.picking_id.sale_id.mapped('order_line').filtered(lambda a : a.product_id.id == item.product_id.id).price_unit
+
+            price_unit = item.sale_id.mapped('order_line').filtered(lambda
+                                                                        a: a.product_id.id == item.product_id.id).price_unit if self.is_multiple_dispatch else item.picking_id.sale_id.mapped(
+                'order_line').filtered(lambda a: a.product_id.id == item.product_id.id).price_unit
             amount = item.real_dispatch_qty * price_unit if self.is_multiple_dispatch else item.quantity_done * price_unit
-            tax_ids = self.sale_id.mapped('order_line').filtered(lambda a : a.product_id.id == item.product_id.id).tax_id
+            tax_ids = self.sale_id.mapped('order_line').filtered(lambda a: a.product_id.id == item.product_id.id).tax_id
             amount_tax = 0
             for tax in tax_ids:
                 if int(tax.amount) == 19:
                     amount_tax += amount * (tax.amount / 100)
                     break
 
-            if len(self.sale_id.mapped('order_line').filtered(lambda a : a.product_id.id == item.product_id.id).mapped('tax_id')) == 0:
+            if len(self.sale_id.mapped('order_line').filtered(lambda a: a.product_id.id == item.product_id.id).mapped(
+                    'tax_id')) == 0:
                 haveExempt = True
 
             if haveExempt:
@@ -253,14 +260,18 @@ class StockPicking(models.Model):
                         "ProductTypeCode": "EAN",
                         "ProductCode": str(item.product_id.default_code),
                         "ProductName": item.product_id.name,
-                        "ProductQuantity": str(item.real_dispatch_qty) if self.is_multiple_dispatch else str(item.quantity_done), #segun DTEmite no es requerido int
+                        "ProductQuantity": str(item.real_dispatch_qty) if self.is_multiple_dispatch else str(
+                            item.quantity_done),
+                        # segun DTEmite no es requerido int
                         "UnitOfMeasure": str(item.product_id.uom_id.name),
-                        "ProductPrice": str(price_unit), #segun DTEmite no es requerido int
+                        "ProductPrice": str(price_unit),  # segun DTEmite no es requerido int
                         "ProductDiscountPercent": "0",
                         "DiscountAmount": "0",
-                        "Amount": str(self.roundclp(item.real_dispatch_qty * price_unit if self.is_multiple_dispatch else item.quantity_done * price_unit)), #str(int(amount)),
+                        "Amount": str(self.roundclp(
+                            item.real_dispatch_qty * price_unit if self.is_multiple_dispatch else item.quantity_done * price_unit)),
+                        # str(int(amount)),
                         "HaveExempt": haveExempt,
-                        "TypeOfExemptEnum": "1" #agregar campo a sale.order.line igual que acoount.invoice.line
+                        "TypeOfExemptEnum": "1"  # agregar campo a sale.order.line igual que acoount.invoice.line
                     }
                 )
             else:
@@ -271,16 +282,18 @@ class StockPicking(models.Model):
                         "ProductTypeCode": "EAN",
                         "ProductCode": str(item.product_id.default_code),
                         "ProductName": item.product_id.name,
-                        "ProductQuantity": str(item.real_dispatch_qty) if self.is_multiple_dispatch else str(item.quantity_done),
+                        "ProductQuantity": str(item.real_dispatch_qty) if self.is_multiple_dispatch else str(
+                            item.quantity_done),
                         "UnitOfMeasure": str(item.product_id.uom_id.name),
                         "ProductPrice": str(price_unit),
                         "ProductDiscountPercent": "0",
                         "DiscountAmount": "0",
-                        "Amount": str(self.roundclp(item.real_dispatch_qty * price_unit if self.is_multiple_dispatch else item.quantity_done * price_unit)),
+                        "Amount": str(self.roundclp(
+                            item.real_dispatch_qty * price_unit if self.is_multiple_dispatch else item.quantity_done * price_unit)),
                     }
                 )
             lineNumber += 1
-        
+
         if self.partner_id.phone:
             recipientPhone = str(self.partner_id.phone)
         elif self.partner_id.mobile:
@@ -288,24 +301,23 @@ class StockPicking(models.Model):
         else:
             recipientPhone = ''
 
-
         self.write({
             'net_amount': str(self.roundclp(netAmount)),
             'exempt_amount': str(self.roundclp(exemtAmount)),
             'amount_tax': str(self.roundclp(amount_tax)),
             'total': str(self.roundclp(netAmount + exemtAmount + self.sale_id.amount_tax))
         })
-     
 
-        invoice= {
+        invoice = {
             "dteType": self.dte_type_id.code,
             "createdDate": self.scheduled_date.strftime("%Y-%m-%d"),
-            "expirationDate": self.date_due.strftime("%Y-%m-%d"), #No hay fecha de vencimiento
+            "expirationDate": self.date_due.strftime("%Y-%m-%d"),  # No hay fecha de vencimiento
             "paymentType": int(self.method_of_payment),
             "dispatchType": str(self.dispatch_type),
             "transferIndication": str(self.transfer_indication),
             "transmitter": {
-                "EnterpriseRut": re.sub('[\.]','', self.company_id.invoice_rut), #re.sub('[\.]','', "76.991.487-0"), #self.env.user.company_id.invoice_rut,
+                "EnterpriseRut": re.sub('[\.]', '', self.company_id.invoice_rut),
+                # re.sub('[\.]','', "76.991.487-0"), #self.env.user.company_id.invoice_rut,
                 "EnterpriseActeco": self.company_activity_id.code,
                 "EnterpriseAddressOrigin": self.env.user.company_id.street,
                 "EnterpriseCity": self.env.user.company_id.city,
@@ -315,7 +327,7 @@ class StockPicking(models.Model):
                 "EnterprisePhone": self.env.user.company_id.phone if self.env.user.company_id.phone else ''
             },
             "recipient": {
-                "EnterpriseRut": re.sub('[\.]','', self.partner_id.invoice_rut),
+                "EnterpriseRut": re.sub('[\.]', '', self.partner_id.invoice_rut),
                 "EnterpriseAddressOrigin": self.partner_id.street[0:60],
                 "EnterpriseCity": self.partner_id.city,
                 "EnterpriseCommune": self.partner_id.state_id.name,
@@ -324,15 +336,15 @@ class StockPicking(models.Model):
                 "EnterprisePhone": recipientPhone
             },
             "total": {
-                "netAmount": self.net_amount, 
+                "netAmount": self.net_amount,
                 "exemptAmount": self.exempt_amount,
                 "taxRate": "19",
                 "taxtRateAmount": self.amount_tax,
-                "totalAmount":self.total 
+                "totalAmount": self.total
             },
             "lines": productLines,
         }
-        
+
         # Add Refeences
         if self.references and len(self.references) > 0:
             refrenecesList = []
@@ -350,45 +362,45 @@ class StockPicking(models.Model):
                 )
                 line_reference_number += 1
             invoice['references'] = refrenecesList
-        #Add Additionals
+        # Add Additionals
         if len(self.observations_ids) > 0:
             additionals = []
             for item in self.observations_ids:
                 additionals.append(item.observations)
-            invoice['additional'] =  additionals    
-
+            invoice['additional'] = additionals
 
         r = requests.post(url, json=invoice, headers=headers)
 
-        #raise models.ValidationError(json.dumps(invoice))
+        # raise models.ValidationError(json.dumps(invoice))
         jr = json.loads(r.text)
         Jrkeys = jr.keys()
         if 'urlPdf' in Jrkeys and 'filePdf' in Jrkeys and 'folio' in Jrkeys and 'fileXml' in Jrkeys and 'ted' in Jrkeys:
-            self.write({'pdf_url':jr['urlPdf']})
-            self.write({'dte_pdf':jr['filePdf']})
-            self.write({'dte_folio':jr['folio']})
-            self.write({'dte_xml':jr['fileXml']})
-            self.write({'dte_xml_sii':jr['fileXmlSII']})
+            self.write({'pdf_url': jr['urlPdf']})
+            self.write({'dte_pdf': jr['filePdf']})
+            self.write({'dte_folio': jr['folio']})
+            self.write({'dte_xml': jr['fileXml']})
+            self.write({'dte_xml_sii': jr['fileXmlSII']})
 
             cols = 12
             while True:
                 try:
                     if cols == 31:
                         break
-                    codes = encode(jr['ted'],cols)
+                    codes = encode(jr['ted'], cols)
                     image = render_image(codes, scale=5, ratio=2)
                     buffered = BytesIO()
                     image.save(buffered, format="JPEG")
                     img_str = base64.b64encode(buffered.getvalue())
-                    self.write({'ted':img_str})
+                    self.write({'ted': img_str})
                     break
                 except:
                     cols += 1
-      
+
         if 'status' in Jrkeys and 'title' in Jrkeys:
-            raise models.ValidationError('Status: {} Title: {} Json: {}'.format(jr['status'],jr['title'],json.dumps(invoice)))
+            raise models.ValidationError(
+                'Status: {} Title: {} Json: {}'.format(jr['status'], jr['title'], json.dumps(invoice)))
         elif 'message' in Jrkeys:
-            raise models.ValidationError('Advertencia: {} Json: {}'.format(jr['message'],json.dumps(invoice)))
+            raise models.ValidationError('Advertencia: {} Json: {}'.format(jr['message'], json.dumps(invoice)))
 
     def roundclp(self, value):
         value_str = str(value)
@@ -428,7 +440,7 @@ class StockPicking(models.Model):
         if len(self.references) > 10:
             raise models.ValidationError('Solo puede generar 20 Referencias')
 
-        if len(self.observations_ids) > 10: 
+        if len(self.observations_ids) > 10:
             raise models.ValidationError('Solo puede generar 10 Observaciones')
 
         valid_to_sii = True
