288a7149809479748a48478661b8c7ea1bf5b5cc
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2023-02-28 17:32:07 -0300

    req 6

--- dimabe_manufacturing/models/report_raw_lot.py
+++ dimabe_manufacturing/models/report_raw_lot.py
@@ -171,21 +171,33 @@ class ReportRawLot(models.Model):
             col += 1
             if r_lot.send_to_process_id:
                 sheet.write(row, col, r_lot.send_to_process_id.display_name, self.get_format('text', workbook))
+            else:
+                sheet.write(row, col, '', self.get_format('text', workbook))
             col += 1
             if r_lot.send_date:
                 sheet.write(row, col, r_lot.send_date, self.get_format('datetime', workbook))
+            else:
+                sheet.write(row, col, '', self.get_format('text', workbook))
             col += 1
             if r_lot.warehouse:
                 sheet.write(row, col, r_lot.warehouse, self.get_format('text', workbook))
+            else:
+                sheet.write(row, col, '', self.get_format('text', workbook))
             col += 1
             if r_lot.street:
                 sheet.write(row, col, r_lot.street, self.get_format('text', workbook))
+            else:
+                sheet.write(row, col, '', self.get_format('text', workbook))
             col += 1
             if r_lot.packaging_qty > 0:
                 sheet.write(row, col, r_lot.packaging_qty, self.get_format('text', workbook))
+            else:
+                sheet.write(row, col, '', self.get_format('text', workbook))
             col += 1
             if r_lot.position > 0:
                 sheet.write(row, col, r_lot.position, self.get_format('text', workbook))
+            else:
+                sheet.write(row, col, '', self.get_format('text', workbook))
             col = 0
             row += 1
         sheet.autofit()
