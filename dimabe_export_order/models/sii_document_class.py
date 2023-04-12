ad853511fa976c1d4c84d18313b607db03ab46f6
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2023-04-12 14:27:32 -0400

    [FIX] change to do_action in custom_in_screen widget and change order to exectuion on delete serial

--- dimabe_export_order/models/sii_document_class.py
+++ dimabe_export_order/models/sii_document_class.py
@@ -17,8 +17,9 @@ class SiiDocumentClass(models.Model):
                 return current_document_number
 
     def verify_sii_document_number(self, document_number):
+
         last_caf_charged = max(caf.final_nm for caf in
                                self.env['dte.caf'].sudo().search([('sii_document_class', '=', self.sii_code)]))
-        if document_number > last_caf_charged:
+        if int(document_number) > last_caf_charged:
             raise models.UserError(
                 'No cuenta con folio disponible para la tipo de documento {}'.format(self.display_name))
