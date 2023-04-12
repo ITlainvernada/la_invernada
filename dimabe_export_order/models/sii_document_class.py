4ca9a1db853b79a2e17d421c37d22191eecc8b19
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2023-04-12 11:55:47 -0400

    check serial_harvest

--- dimabe_export_order/models/sii_document_class.py
+++ dimabe_export_order/models/sii_document_class.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class SiiDocumentClass(models.Model):
@@ -8,11 +8,17 @@ class SiiDocumentClass(models.Model):
 
     def get_last_caf_consumed(self):
         for item in self:
-            items = []
-            print(type(item.sii_code))
             if item.sii_code == 52:
                 records = self.env['stock.picking'].sudo().search(
                     [('use_documents', '=', True), ('location_id.sii_document_class_id.id', '=', item.id),
                      ('state', '=', 'done')]).filtered(lambda x: x.sii_result not in ['NoEnviado', '']).mapped(
                     'sii_document_number')
-            return max(int(record) for record in records)
+                current_document_number = max(int(record) for record in records) + 1
+                return current_document_number
+
+    def verify_sii_document_number(self, document_number):
+        last_caf_charged = max(caf.final_nm for caf in
+                               self.env['dte.caf'].sudo().search([('sii_document_class', '=', self.sii_code)]))
+        if document_number > last_caf_charged:
+            raise models.UserError(
+                'No cuenta con folio disponible para la tipo de documento {}'.format(self.display_name))
