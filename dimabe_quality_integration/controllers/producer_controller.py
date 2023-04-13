e4c8d0f44496aafbbcf5716a27d1d54f6f502cfe
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2023-04-13 18:35:46 -0400

    [FIX] out_weight in mrp_workorder on delete serial or pallet

--- dimabe_quality_integration/controllers/producer_controller.py
+++ dimabe_quality_integration/controllers/producer_controller.py
@@ -8,7 +8,7 @@ class producer_controller(http.Controller):
 
     @http.route('/api/vat_producer_by_lot', type='json', methods=['GET'], auth='token', cors='*')
     def get_producer_by_lot(self, lot=None):
-        partner_id = self.env['stock.production.lot'].search([('name', '=', lot)]).producer_id
+        partner_id = request.env['stock.production.lot'].search([('name', '=', lot)]).producer_id
         vat = ''
         if partner_id:
             vat = partner_id.document_number if partner_id.document_number else partner_id.client_identifier_value
