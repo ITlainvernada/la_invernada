7cbb870bf53f3640d01a7bf2cfa269b538612b7b
= <esteban.mallia@dimabe.cl>
2023-04-14 12:37:02 -0400

    update api to get vat producers by lots

--- dimabe_quality_integration/controllers/producer_controller.py
+++ dimabe_quality_integration/controllers/producer_controller.py
@@ -6,8 +6,8 @@ import werkzeug
 class producer_controller(http.Controller):
 
     @http.route('/api/vat_producer_by_lot', type='json', methods=['GET'], auth='token', cors='*')
-    def get_producer_by_lot(self, lots=[]):
-        lot_ids = request.env['stock.production.lot'].search([('name', 'in', lots)])
+    def get_producer_by_lot(self, year=None):
+        lot_ids = request.env['stock.production.lot'].search([('harvest', '=', year)])
         data = []
 
         for lot_id in lot_ids:
