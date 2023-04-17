da6b8a9819c3496996d1d4332b8d321e7faf8874
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2023-04-17 15:38:25 -0400

    fix skip_document_number

--- dimabe_manufacturing/static/src/js/custom_in_screen.js
+++ dimabe_manufacturing/static/src/js/custom_in_screen.js
@@ -10,20 +10,18 @@ odoo.define('dimabe_manufacturing.barcode_manager', function (require) {
             this._super.apply(this, arguments);
         },
         _barcodeScanned: function (barcode, target) {
-            var self = this
-            console.log(barcodesForm);
-            // rpc.query({
-            //     model: this.modelName,
-            //     method: 'process_serial_by_barcode',
-            //     args: [barcode, this.initialState.data.id]
-            // }).then(res => {
-            //     if (!res.ok && !res.name) {
-            //         self.do_warn(res.message);
-            //     } else {
-            //         self.do_action(res);
-            //     }
-            //
-            // })
+            rpc.query({
+                model: this.modelName,
+                method: 'process_serial_by_barcode',
+                args: [barcode, this.initialState.data.id]
+            }).then(res => {
+                if (!res.ok && !res.name) {
+                    self.do_warn(res.message);
+                } else {
+                    self.do_action(res);
+                }
+
+            })
         }
     })
 })
