1e7ae8e4e2fbee7259a06a9e03cbd7e2a8bf004c
Felipe Angulo <felipe@mobilize.cl>
2023-06-07 08:57:09 -0400

    [IMP] api integration controllers (stock.picking)

--- mblz_picking_endpoints/controllers/main.py
+++ mblz_picking_endpoints/controllers/main.py
@@ -9,7 +9,9 @@ class StockPickingController(http.Controller):
     def get_pickings(self):
         token = request.httprequest.headers['AUTHORIZATION'].split(' ')[1]
         if token and token == request.env['ir.config_parameter'].sudo().get_param('mblz_picking_endpoints.token'):
+            domain = []
+            picking_ids = request.env['stock.picking'].sudo().search(domain, limit=100)
             return json.dumps([{
-                'name': 'P0024',
-                'state': 'done'
-            }], indent=1, ensure_ascii=False).encode('utf8')
+                'name': picking_id.name,
+                'state': picking_id.state
+            } for picking_id in picking_ids], ensure_ascii=False).encode('utf8')
