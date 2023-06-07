289c858ea9a5807b795ae414afd05e8055be0024
Felipe Angulo <felipe@mobilize.cl>
2023-06-07 12:20:44 -0400

    [IMP] receptions endpoint v2, add domain

---
+++ mblz_picking_endpoints/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Endpoints consulta pickings",
+
+    'summary': """
+       Endpoints consulta pickings""",
+
+    'description': """
+        Endpoints consulta pickings
+    """,
+
+    'author': "Mobilize SPA, Felipe Angulo",
+    'website': "http://www.mobilize.cl",
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'version': '0.1',
+    'depends': [
+        'base'
+        ],
+    'data': [
+        # 'security/ir.model.access.csv'
+    ],
+    'demo': [],
+    'images': [
+        'static/decription/icon.png'
+        ],
+    'application': True,
+    'installable': True,
+   
+}
---
+++ mblz_picking_endpoints/controllers/main.py
@@ -0,0 +1,66 @@
+from odoo import http
+from odoo.exceptions import UserError
+from odoo.http import request
+import json
+
+class StockPickingController(http.Controller):
+    def _get_container_info(self, moves, key):
+        for move in moves:
+            if not move.variety:
+                if key == 'product_name':
+                    return move.product_id.name
+                return move[key]
+            
+    def _get_variety_info(self, moves, key):
+        names = []
+        for move in moves:
+            if key in ['default_code', 'display_name']:
+                names.append(move.product_id[key])
+            elif move[key]:
+                names.append(move[key])
+        return '/'.join(names)
+    
+    def _get_picking_data(self, picking_ids):
+        return [{
+                'ID_SQL': picking_id.id,
+                'ProducerCode': picking_id.sag_code,
+                'ProducerName': picking_id.partner_id.name,
+                'VarietyName': self._get_variety_info(picking_id.move_ids_without_package, 'variety'),
+                'LotNumber': picking_id.name,
+                'DispatchGuideNumber': picking_id.guide_number,
+                'ReceptionDate': picking_id.date_done.strftime('%Y-%m-%d %H:%M:%S'),
+                'ReceptionKgs': picking_id.gross_weight,
+                'Season': picking_id.harvest,
+                'QualityNumber': 'N/A',
+                'Warehouse': picking_id.location_dest_id.name,
+                'QualityWeight': picking_id.quality_weight,
+                'ContainerQuantity': self._get_container_info(picking_id.move_ids_without_package, 'quantity_done'),
+                'ContainerWeightAverage': picking_id.avg_unitary_weight,
+                'Observation': picking_id.note,
+                'Tare': picking_id.tare_weight,
+                'ContainerType': self._get_container_info(picking_id.move_ids_without_package, 'product_name'),
+                'ArticleCode': self._get_variety_info(picking_id.move_ids_without_package, 'default_code'),
+                'ArticleDescription': self._get_variety_info(picking_id.move_ids_without_package, 'display_name'),
+                'DryKgs': 'N/A',
+                'QualityGreenId': 'N/A',
+                'ContainerWeight': 'N/A',
+                'OdooUpdated': picking_id.write_date.strftime('%Y-%m-%d %H:%M:%S'),
+                'UpdatedAt': 'N/A'  
+            } for picking_id in picking_ids]
+
+    @http.route('/api/v2/pickings', type='json', methods=['POST'], auth='public', cors='*')
+    def get_pickings(self):
+        token = request.httprequest.headers['AUTHORIZATION'].split(' ')[1]
+        if token and token == request.env['ir.config_parameter'].sudo().get_param('mblz_picking_endpoints.token'):
+            domain = [('state', 'in', ['done']), ('picking_type_code', '=', 'incoming')]
+            picking_ids = request.env['stock.picking'].sudo().search(domain, limit=100)
+            return json.dumps(self._get_picking_data(picking_ids), ensure_ascii=False)
+    
+    @http.route('/api/v2/picking_ids', type='json', methods=['POST'], auth='public', cors='*')
+    def get_pickings(self):
+        token = request.httprequest.headers['AUTHORIZATION'].split(' ')[1]
+        if token and token == request.env['ir.config_parameter'].sudo().get_param('mblz_picking_endpoints.token'):
+            domain = [('state', 'in', ['done']), ('picking_type_code', '=', 'incoming')]
+            picking_ids = request.env['stock.picking'].sudo().search(domain, limit=100)
+            return json.dumps({
+                'ids': picking_ids.ids}, ensure_ascii=False)
